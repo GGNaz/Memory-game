a7f36a709a07f9c016d639b3588140546f3d0879
ggnaz <nazersomera123@gmail.com>
2022-11-13 07:38:25 +0700

    saved

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ import fanny from "./Assets/fanny.jpg";
 import gusion from "./Assets/gusion.jpg";
 import diggie from "./Assets/diggie.jpg";
 import nata from "./Assets/nata.jpg";
-
+import thamuz from "./Assets/thamuz.jpg";
 const App = () => {
   const [loadCards, setLoadCards] = useState([]);
   const [tempStore, setTempStore] = useState([]);
   const [openModal, setOpenModal] = useState(false);
+  const [showModal, setShowModal] = React.useState(false);
   let x = 10;
 
   // getRandomInt(x)
@@ -58,7 +59,7 @@ const App = () => {
         },
         {
           name: "E",
-          card: ( <img src={fanny} alt="fanny" className="h-50 w-full" />),
+          card: ( <img src={thamuz} alt="thamuz" className="h-50 w-full" />),
         },
         // {
         //   name: "F",
@@ -199,7 +200,7 @@ const App = () => {
         checkIfWin = checkIfWin + 1;
       }
       if (checkIfWin === loadCards?.length && loadCards?.length === i + 1) {
-        alert("YOU WIN MADA FAKAH");
+        setShowModal(true)
         // return win = true
       }
     });
@@ -231,7 +232,60 @@ const App = () => {
   }
 
   const alertModal = () => {
-
+    return (
+      <>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-md min-w-fit">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="font-sans">
+                    You won!
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                   Do you wanna play again?
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Yes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </> )
   }
 
   const time = new Date();
@@ -240,6 +294,7 @@ const App = () => {
   return (
     <div className="bg-slate-700 min-h-full">
       {alertModal()}
+     
       {/* <GameTimer expiryTimestamp={time} /> */}
       <div className="flex flex-row justify-center "><img style={{ height: "200px" }} src={mlbbLogo} alt="mlbblogo" /></div>
       <div className="grid grid-cols-5 gap-4">
